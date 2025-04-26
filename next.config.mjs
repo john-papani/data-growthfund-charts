073e2d1eb2c21b8a0dcb5a14ea6be1c3c0031b85
john-papani <johnpapani1@gmail.com>
2025-04-26 19:16:35 +0300

    Fix isprod next.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === "PRODUCTION";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: false,
   output: "export",
   basePath: isProd ? "/hcapdata-charts" : "",
-  assetPrefix: isProd ? "/hcapdata-charts/" : "", 
+  assetPrefix: isProd ? "/hcapdata-charts/" : "",
 };
 
 export default nextConfig;
