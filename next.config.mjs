0bbfc986e125ce3c886ad64ad12e198c75e32681
john-papani <johnpapani1@gmail.com>
2025-05-18 16:56:26 +0300

    Change next config file for name change

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,13 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: false,
   output: "export",
-  basePath: isProd ? "/hcapdata-charts" : "",
-  assetPrefix: isProd ? "/hcapdata-charts/" : "",
+  basePath: isProd ? "/data-growthfund-charts" : "",
+  assetPrefix: isProd ? "/data-growthfund-charts/" : "",
   images: {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/hcapdata-charts" : "",
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/data-growthfund-charts" : "",
   },
 };
 
